358eebb0d9a8c57f874209e4573d330ee1afb941
Tshireletso Mpudu <125552061+Tshireletso46@users.noreply.github.com>
2024-01-25 22:54:28 +0200

    updated documentation

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// This function clear all the values
+// Clears all the values entered or calculated by setting the result input value to an empty string.
 function clearScreen() {
     document.getElementById("result").value = "";
 }
 
-// This function display values
+// Appends the provided value to the current content of the result input.
 function display(value) {
     document.getElementById("result").value += value;
 }
 
-// This function evaluates the expression and returns result
+// Function to evaluate the mathematical expression and display the result on the calculator screen.
 function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
+
+    
+    // Set the result input value to the calculated result, updating the screen.
     document.getElementById("result").value = q;
 }
